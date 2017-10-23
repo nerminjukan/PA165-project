3e46a1dfbcc99f960aef590783df009f0beb0915
kotycheese <pavel.kotala@gnjcz>
2017-10-23 22:28:16 +0200

    Trip, TripDAO, TripDAOImpl created

--- src/main/java/fi/muni/cz/pa165/travelagency/dao/TripDaoImpl.java
+++ src/main/java/fi/muni/cz/pa165/travelagency/dao/TripDaoImpl.java
@@ -1,7 +1,55 @@
 package fi.muni.cz.pa165.travelagency.dao;
 
+import fi.muni.cz.pa165.travelagency.entity.Excursion;
+import fi.muni.cz.pa165.travelagency.entity.Trip;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+import java.util.List;
+
 /**
- * Created by pavelkotala on 23.10.2017.
+ * @author Pavel Kotala, 437164
  */
-public class TripDaoImpl {
+public class TripDaoImpl implements TripDao {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Trip trip) {
+        em.persist(trip);
+    }
+
+    @Override
+    public List<Trip> findAll() {
+        TypedQuery<Trip> query = em.createQuery("SELECT t FROM Trip t", Trip.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Trip> findByName(String name) {
+        TypedQuery<Trip> query = em.createQuery("SELECT t FROM Trip t WHERE t.name = :name", Trip.class);
+        query.setParameter("name", name);
+        return query.getResultList();
+    }
+
+    @Override
+    public Trip findById(Long id) {
+        return em.find(Trip.class, id);
+    }
+
+    @Override
+    public void remove(Trip trip) {
+        em.remove(trip);
+    }
+
+    @Override
+    public List<Trip> getTripsBetween(Date start, Date end) {
+        TypedQuery<Trip> query = em.createQuery("SELECT t FROM Trip t WHERE t.dateFrom BETWEEN :start AND :end AND " +
+                "t.dateTo BETWEEN :start AND :end", Trip.class);
+        query.setParameter("start", start);
+        query.setParameter("end", end);
+        return query.getResultList();
+    }
 }
