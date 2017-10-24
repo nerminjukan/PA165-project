f296f89ecc5da2c106f61d4825c2221d35af613b
445611 <445611@mail.muni.cz>
2017-10-24 16:24:21 +0200

    Application context prepared for test, Dependecies added

---
+++ src/main/java/fi/muni/cz/pa165/travelagency/PersistenceSampleApplicationContext.java
@@ -0,0 +1,89 @@
+package fi.muni.cz.pa165.travelagency;
+
+import fi.muni.cz.pa165.travelagency.entity.Customer;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.sql.DataSource;
+
+
+/**
+ * Simple persistence application context for tests.
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories
+@ComponentScan(basePackageClasses = {Customer.class}, basePackages = "fi.muni.cz.pa165.travelagency")
+public class PersistenceSampleApplicationContext {
+
+    /**
+     * Enables automatic translation of exceptions to DataAccessExceptions.
+     * @return Bean post-processor to be used.
+     */
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor postProcessor() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    /**
+     * Binds a JPA EntityManager from the specified factory to the thread.
+     * @return New JpaTransactionManager instance with entityManagerFactory.
+     */
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory().getObject());
+    }
+
+    /**
+     * Starts up a container that emulates behavior prescribed in JPA spec for container-managed EntityManager
+     * @return LocalContainerEntityManagerFactoryBean set up.
+     */
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean jpaFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        jpaFactoryBean.setDataSource(db());
+        jpaFactoryBean.setLoadTimeWeaver(instrumentationLoadTimeWeaver());
+        jpaFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        return jpaFactoryBean;
+    }
+
+    /**
+     * The central class for javax.validation.
+     * @return LocalValidatorFactoryBean.
+     */
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    /**
+     * Defines the contract for adding one or more ClassFileTransformers to a ClassLoader.
+     * @return LoadTimeWeaver.
+     */
+    @Bean
+    public LoadTimeWeaver instrumentationLoadTimeWeaver() {
+        return new InstrumentationLoadTimeWeaver();
+    }
+
+    /**
+     * A factory for connections to the physical data source .
+     * @return DataSource with embedded database build.
+     */
+    @Bean
+    public DataSource db() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.DERBY).build();
+    }
+}
