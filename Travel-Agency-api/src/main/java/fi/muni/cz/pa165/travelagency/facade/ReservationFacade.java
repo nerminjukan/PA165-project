186cd751c59d149ae84f3abf84f2267e1609e7d3
445611 <445611@mail.muni.cz>
2017-11-14 01:01:08 +0100

    Made moduls for impl

---
+++ Travel-Agency-api/src/main/java/fi/muni/cz/pa165/travelagency/facade/ReservationFacade.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.muni.cz.pa165.travelagency.facade;
+
+/**
+ *
+ * @author rados
+ */
+public interface ReservationFacade {
+    
+}
