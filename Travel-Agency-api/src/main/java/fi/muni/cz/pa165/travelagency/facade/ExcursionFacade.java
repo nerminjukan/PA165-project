7b4292750739935c6601fbc0dce72a582101bec3
nerminjukan <nj2245@student.uni-lj.si>
2017-11-16 22:08:26 +0000

    ExcursionDAO update

--- Travel-Agency-api/src/main/java/fi/muni/cz/pa165/travelagency/facade/ExcursionFacade.java
+++ Travel-Agency-api/src/main/java/fi/muni/cz/pa165/travelagency/facade/ExcursionFacade.java
@@ -5,10 +5,48 @@
  */
 package fi.muni.cz.pa165.travelagency.facade;
 
+import fi.muni.cz.pa165.travelagency.dto.ExcursionCreateDTO;
+import fi.muni.cz.pa165.travelagency.dto.ExcursionDTO;
+import fi.muni.cz.pa165.travelagency.dto.ExcursionUpdateDTO;
+import java.util.List;
+
+
 /**
  *
  * @author root
  */
 public interface ExcursionFacade {
+    
+    /**
+     *
+     * @param e
+     * @return
+     */
+    public int createExcursion(ExcursionCreateDTO e);
+    
+    /**
+     *
+     * @return
+     */
+    public List<ExcursionDTO> getAllExcursions();
+    
+    /**
+     *
+     * @param excursion
+     * @return
+     */
+    public ExcursionDTO getExcursionByID(Long excursion);
+    
+    /**
+     *
+     * @param excursionId
+     */
+    public void deleteExcursion(Long excursionId);
+    
+    /**
+     *
+     * @param e
+     */
+    public void updateExcursion(ExcursionUpdateDTO e);
     
 }
