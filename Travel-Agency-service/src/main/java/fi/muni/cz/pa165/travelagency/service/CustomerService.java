0da790d837880bfb469c57b3a04d1131a68f9c79
Sevitas <442365@mail.muni.cz>
2017-11-17 16:46:02 +0100

    CustomerService interface + impl

--- Travel-Agency-service/src/main/java/fi/muni/cz/pa165/travelagency/service/CustomerService.java
+++ Travel-Agency-service/src/main/java/fi/muni/cz/pa165/travelagency/service/CustomerService.java
@@ -1,4 +1,52 @@
 package fi.muni.cz.pa165.travelagency.service;
 
+import fi.muni.cz.pa165.travelagency.entity.Customer;
+import fi.muni.cz.pa165.travelagency.entity.Reservation;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Customer service interface
+ * @author Martin Sevcik <422365>
+ */
+@Service
 public interface CustomerService {
+    /**
+     * Create operation for CustomerDao
+     * @param customer Customer
+     */
+    void create(Customer customer);
+
+    /**
+     * Finds all Customers
+     * @return Customer's list
+     */
+    List<Customer> findAll();
+
+    /**
+     * Finds specified customer
+     * @param id id of customer
+     * @return Customer
+     */
+    Customer findById(Long id);
+
+    /**
+     * Removes Customer
+     * @param customer Customer
+     */
+    void remove(Customer customer);
+
+    /**
+     * Updates Customer
+     * @param customer Customer
+     */
+    void update(Customer customer);
+
+    /**
+     * Finds customers based on reservation
+     * @param reservation Reservation
+     * @return Customer
+     */
+    Customer findByReservation(Reservation reservation);
 }
--- Travel-Agency-service/src/main/java/fi/muni/cz/pa165/travelagency/service/CustomerServiceImpl.java
+++ Travel-Agency-service/src/main/java/fi/muni/cz/pa165/travelagency/service/CustomerServiceImpl.java
@@ -1,4 +1,46 @@
 package fi.muni.cz.pa165.travelagency.service;
 
-public class CustomerServiceImpl {
+import fi.muni.cz.pa165.travelagency.dao.CustomerDao;
+import fi.muni.cz.pa165.travelagency.entity.Customer;
+import fi.muni.cz.pa165.travelagency.entity.Reservation;
+import org.dozer.inject.Inject;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Inject
+    private CustomerDao customerDao;
+
+    @Override
+    public void create(Customer customer) {
+        customerDao.create(customer);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerDao.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerDao.findById(id);
+    }
+
+    @Override
+    public void remove(Customer customer) {
+        customerDao.remove(customer);
+    }
+
+    @Override
+    public void update(Customer customer) {
+        customerDao.update(customer);
+    }
+
+    @Override
+    public Customer findByReservation(Reservation reservation) {
+        return customerDao.findByReservation(reservation);
+    }
 }
