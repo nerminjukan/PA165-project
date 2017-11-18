504e41185848f825ae574549836c59b2a7c2d3ea
Sevitas <442365@mail.muni.cz>
2017-11-18 18:10:33 +0100

    beanMappingService impl

--- Travel-Agency-service/src/main/java/fi/muni/cz/pa165/travelagency/service/BeanMappingServiceImpl.java
+++ Travel-Agency-service/src/main/java/fi/muni/cz/pa165/travelagency/service/BeanMappingServiceImpl.java
@@ -2,23 +2,33 @@ package fi.muni.cz.pa165.travelagency.service;
 
 import org.dozer.Mapper;
 import org.hibernate.cfg.NotYetImplementedException;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class BeanMappingServiceImpl implements BeanMappingService {
+
+    @Autowired
+    private Mapper dozer;
+
     @Override
     public <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass) {
-        throw new NotYetImplementedException();
+        List<T> mappedCollection = new ArrayList<>();
+        for (Object object : objects) {
+            mappedCollection.add(dozer.map(object, mapToClass));
+        }
+        return mappedCollection;
     }
 
     @Override
     public <T> T mapTo(Object u, Class<T> mapToClass) {
-        throw new NotYetImplementedException();
+        return dozer.map(u,mapToClass);
     }
 
     @Override
     public Mapper getMapper() {
-        throw new NotYetImplementedException();
+        return dozer;
     }
 }
