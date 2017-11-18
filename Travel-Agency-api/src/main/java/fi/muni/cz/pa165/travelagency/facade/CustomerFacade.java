a5e28e765d4a00a411e9d23da5d2b7814bc9603f
Sevitas <442365@mail.muni.cz>
2017-11-18 17:58:22 +0100

    CustomerFacade interface

--- Travel-Agency-api/src/main/java/fi/muni/cz/pa165/travelagency/facade/CustomerFacade.java
+++ Travel-Agency-api/src/main/java/fi/muni/cz/pa165/travelagency/facade/CustomerFacade.java
@@ -1,8 +1,50 @@
 package fi.muni.cz.pa165.travelagency.facade;
 
+import fi.muni.cz.pa165.travelagency.dto.CustomerDTO;
+
+import java.util.List;
+
 /**
  * Customer Facade class
  * @author Martin Sevcik <422365>
  */
 public interface CustomerFacade {
+
+    /**
+     * Create operation for CustomerDao
+     * @param customer Customer
+     */
+    void create(CustomerDTO customer);
+
+    /**
+     * Finds all Customers
+     * @return Customer's list
+     */
+    List<CustomerDTO> findAll();
+
+    /**
+     * Finds specified customer
+     * @param id id of customer
+     * @return Customer
+     */
+    CustomerDTO findById(Long id);
+
+    /**
+     * Removes Customer
+     * @param customer Customer
+     */
+    void remove(CustomerDTO customer);
+
+    /**
+     * Updates Customer
+     * @param customer Customer
+     */
+    void update(CustomerDTO customer);
+
+    /**
+     * Finds customers based on reservation
+     * @param reservation Reservation
+     * @return Customer
+     */
+    CustomerDTO findByReservation(CustomerDTO reservation);
 }
