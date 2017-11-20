67ba97815b02f5be09f246d681a30bde70e504e7
radoslavmicko <32122288+445611@users.noreply.github.com>
2017-11-20 11:52:19 +0100

    Update TripFacade.java

--- Travel-Agency-api/src/main/java/fi/muni/cz/pa165/travelagency/facade/TripFacade.java
+++ Travel-Agency-api/src/main/java/fi/muni/cz/pa165/travelagency/facade/TripFacade.java
@@ -49,7 +49,6 @@ public interface TripFacade {
     /**
      * *CHANGE ME*
      * @param tripId *CHANGE ME*
-     * @param excursionIds *CHANGE ME*
      */
     void deleteTrip(Long tripId);
     
