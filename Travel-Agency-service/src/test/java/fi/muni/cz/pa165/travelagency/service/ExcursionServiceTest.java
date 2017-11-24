2649feaa4783e47b597f688e99ef5ac968d16df9
nerminjukan <nj2245@student.uni-lj.si>
2017-11-24 01:03:39 +0000

    Started work on tests.

--- Travel-Agency-service/src/test/java/fi/muni/cz/pa165/travelagency/service/ExcursionServiceTest.java
+++ Travel-Agency-service/src/test/java/fi/muni/cz/pa165/travelagency/service/ExcursionServiceTest.java
@@ -1,10 +1,63 @@
 package fi.muni.cz.pa165.travelagency.service;
 
+import fi.muni.cz.pa165.travelagency.dao.ExcursionDao;
+import fi.muni.cz.pa165.travelagency.entity.Customer;
+import fi.muni.cz.pa165.travelagency.entity.Excursion;
+import fi.muni.cz.pa165.travelagency.entity.Reservation;
+import fi.muni.cz.pa165.travelagency.entity.Trip;
+import fi.muni.cz.pa165.travelagency.service.config.ServiceConfiguration;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import org.hibernate.service.spi.ServiceException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeMethod;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+
 /**
  * Excursion Service Tests for the Travel Angecy project.
  * 
  * @author (name = "Nermin Jukan", UCO = "<473370>")
  */
-public class ExcursionServiceTest {
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class ExcursionServiceTest extends AbstractTransactionalTestNGSpringContextTests{
     
+    @Mock
+    private ExcursionDao excursionDao;
+    
+    @Autowired
+    @InjectMocks
+    private ExcursionService excursionService;
+    
+    @BeforeClass
+    public void setup() throws ServiceException{
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    private Excursion excursion;
+    
+    @BeforeMethod
+    public void prepareTest(){
+        Trip trip = new Trip();
+        Reservation reservation = new Reservation();
+        Customer customer = new Customer();
+        
+        Calendar calendar =  Calendar.getInstance();
+        calendar.set(2012, 4, 20);
+        
+        excursion = new Excursion("castle", "france", 2, calendar.getTime(), BigDecimal.valueOf(1999), trip);
+    }
+    
+    @Test
+    public void createExcursionTest(){
+        excursionService.create(excursion);
+        verify(excursionDao).create(excursion);
+    }
 }
