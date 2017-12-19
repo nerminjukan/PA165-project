0a45da5ccaed2dc89ec88981d75c5dfc3b25164b
445611 <445611@muni.cz>
2017-12-19 04:59:35 +0100

    minor security change

--- Travel-Agency-spring-mvc/src/main/java/fi/muni/cz/pa165/travelagency/controllers/ExcursionController.java
+++ Travel-Agency-spring-mvc/src/main/java/fi/muni/cz/pa165/travelagency/controllers/ExcursionController.java
@@ -49,11 +49,23 @@ public class ExcursionController {
      * Shows a list of excursions with the ability to add, delete or edit.
      *
      * @param model data to display
+     * @param request http request
+     * @param redirectAttributes redirection attributes
      * @return JSP page name
      */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String listAll(Model model) {
-        model.addAttribute("excursions", excursionFacade.getAllExcursions());
+    public String listAll(Model model, HttpServletRequest request,
+            RedirectAttributes redirectAttributes) {
+        UserDTO authUser = (UserDTO) request.getSession().getAttribute("authenticatedUser");
+        if (authUser != null) {
+            model.addAttribute("excursions", excursionFacade.getAllExcursions());
+        } else {
+            redirectAttributes.addFlashAttribute("alert_danger",
+                        "Unanthorized.");
+            return "redirect:/auth/login";
+        }
+        
+        
         return "excursion/list";
     }
     
